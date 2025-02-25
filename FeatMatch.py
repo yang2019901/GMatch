2bef23236dc65d08caa93f124866090eb06f3668
yang2019901 <1308592371@qq.com>
2025-02-25 19:20:20 +0800

    use numpy vectorization to speed up Mh calc from 3 sec to 0.03 sec

--- FeatMatch.py
+++ FeatMatch.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import pickle
 
 np.random.seed(0)
+_ham_tab = np.array([bin(i).count("1") for i in range(256)], dtype=np.uint8)
 
 
 def plot_matches(img1, img2, kp1, kp2, matches):
@@ -32,15 +33,18 @@ def plot_matches(img1, img2, kp1, kp2, matches):
 
 
 def HamDist(a, b):
-    return np.sum([bin(a[i] ^ b[i]).count("1") for i in range(len(a))])
+    rlt = 0
+    for i in range(len(a)):
+        rlt += _ham_tab[a[i] ^ b[i]]
+    return rlt
 
 
-def tree_search(kp1, kp2, des1, des2, cld1, cld2):
+def tree_search(kp1, kp2, cld1, cld2, Mh):
     D = 8  # search depth
     L = 8
     K = 8
     matches = []
-    thresh_loss = 0.01  # if the average loss of adding `m` to matches is less than this, accept `m`. unit: meter
+    thresh_loss = 0.015  # if the average loss of adding `m` to matches is less than this, accept `m`. unit: meter
 
     def loss(m):
         res = 0
@@ -52,11 +56,7 @@ def tree_search(kp1, kp2, des1, des2, cld1, cld2):
             res += np.abs(np.linalg.norm(p1_1 - p2_1) - np.linalg.norm(p1_2 - p2_2))
         return res
 
-    good = []
-    for i in range(len(kp1)):
-        for j in range(len(kp2)):
-            if HamDist(des1[i], des2[j]) < 40:
-                good.append((i, j))
+    good = np.argwhere(Mh < 40)
 
     for i, j in good:
         matches.append((i, j))
@@ -71,7 +71,7 @@ def tree_search(kp1, kp2, des1, des2, cld1, cld2):
             tmp = []
             # find top K closest keypoints in kp2
             for idx in indices:
-                dists = [HamDist(des1[idx], des2[j]) for j in range(len(kp2))]
+                dists = Mh[idx, :]
                 closest = np.argsort(dists)[:K]
                 tmp += [(idx, j) for j in closest]
             # find the best match
@@ -90,40 +90,52 @@ def tree_search(kp1, kp2, des1, des2, cld1, cld2):
         else:
             matches.clear()
 
-    print(f"Average loss: {d_sum / D}")
+    if len(matches) < D:
+        print("Not enough matches found")
+    print(f"Average loss: {d_sum * 2 / (D * (D - 1)):.3f}")
     return [cv2.DMatch(i, j, 0) for i, j in matches]
+
+
+def GetHammingMat(des1, des2):
+    """compute hamming distance matrix `Mh` between two descriptors
+    Mh[i, j] == HamDist(des1[i], des2[j])
+    """
+    global _ham_tab
+    ## broadcast des1 and des2
+    des1_ = des1[:, np.newaxis, :]
+    des2_ = des2[np.newaxis, :, :]
+
+    ## compute xor result
+    xor_result = des1_ ^ des2_
+
+    ## compute hamming distance
+    hamming_distances = _ham_tab[xor_result]
+
+    ## sum along the last axis to get the hamming distance matrix
+    Mh = np.sum(hamming_distances, axis=-1)
+
+    return Mh
 
 
 def match_features(img1, img2, cld1, cld2, mask1=None, mask2=None):
     detector: cv2.ORB = cv2.ORB_create()
     # detector.setMaxFeatures(500)
-    # find the keypoints and descriptors with detector
+    ## find the keypoints and descriptors with detector
     kp1, des1 = detector.detectAndCompute(img1, mask1)
     kp2, des2 = detector.detectAndCompute(img2, mask2)
     print(f"Number of keypoints in img1: {len(kp1)}, img2: {len(kp2)}")
 
-    matches = tree_search(kp1, kp2, des1, des2, cld1, cld2)
-    # matches = []
+    t0 = time.time()
+    Mh = GetHammingMat(des1, des2)
+    print(f"Time cost of searching good: {time.time() - t0:.3f}s")
 
-    # def plot(i):
-    #     li = np.array([HamDist(des1[i], des2[j]) for j in range(len(kp2))])
-    #     # sort the distances
-    #     ind = np.argsort(li)
-    #     plt.plot(li[ind])
-    #     # print(li[ind[:10]])
-    #     for j in range(10):
-    #         if li[ind[j]] < 40:
-    #             matches.append(cv2.DMatch(i, ind[j], 0))
-    #             print(f"Matched {i} with {ind[j]}")
+    matches = tree_search(kp1, kp2, cld1, cld2, Mh)
 
-    # for i in range(len(kp1)):
-    #     plot(i)
-
-    img = cv2.drawMatches(img1, kp1, img2, kp2, matches, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-    plt.figure()
-    plt.imshow(img)
-    plt.show()
-    exit(0)
+    # img = cv2.drawMatches(img1, kp1, img2, kp2, matches, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    # plt.figure()
+    # plt.imshow(img)
+    # plt.show()
+    # exit(0)
 
     # draw the keypoints
     img1_with_keypoints = cv2.drawKeypoints(img1, kp1, None, color=(0, 255, 0))
@@ -150,7 +162,7 @@ if __name__ == "__main__":
     # img1 = cv2.resize(img1, (480, 640))
     # img2 = cv2.resize(img2, (480, 640))
 
-    imgs, clds, poses = pickle.load(open("carbinet.pt", "rb"))
-    img1, img2, cld1, cld2 = imgs[0], imgs[1], clds[0], clds[1]
+    imgs, clds, poses = pickle.load(open("carbinet_eval.pt", "rb"))
+    img1, img2, cld1, cld2 = imgs[3], imgs[0], clds[3], clds[0]
 
     match_features(img1, img2, cld1, cld2)
